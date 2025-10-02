9bd504719740dd07130cdd794e6feccf05531994
lefv <luis@lefv.io>
2025-10-02 21:02:03 +0200

    Add debug logging to SceneToggleWatcher
    Track canMove and canAtack state changes when opening/closing inventory and attribute scenes.
    This will help identify if scene toggling is properly restoring player abilities.

--- src/scenes/watchers/SceneToggleWatcher.ts
+++ src/scenes/watchers/SceneToggleWatcher.ts
@@ -4,14 +4,18 @@ import { Player } from '../../entities/Player';
 export class SceneToggleWatcher {
 	static toggleScene(sceneContext: Phaser.Scene, name: string, player: Player): void {
 		if (!sceneContext.scene.isVisible(name)) {
+			console.log(`[SceneToggleWatcher] Opening scene: ${name}`);
 			sceneContext.scene.launch(name, {
 				player: player,
 			});
 			player.canMove = false;
+			console.log(`[SceneToggleWatcher] Scene opened - canMove: ${player.canMove}, canAtack: ${player.canAtack}`);
 		} else {
+			console.log(`[SceneToggleWatcher] Closing scene: ${name} - Before: canMove: ${player.canMove}, canAtack: ${player.canAtack}`);
 			sceneContext.scene.get(name).scene.stop();
 			player.canMove = true;
 			player.canAtack = true;
+			console.log(`[SceneToggleWatcher] Scene closed - After: canMove: ${player.canMove}, canAtack: ${player.canAtack}`);
 		}
 	}
 }
